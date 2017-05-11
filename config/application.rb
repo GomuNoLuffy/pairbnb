f111d881ef354c993b9e60237d9bee1b7ac82e3d
teeyingyap <yapteeying@gmail.com>
2017-05-11 19:31:26 +0800

    7th try

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,6 @@ module PairBnB
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
   end
 end
