7b9a61e2817ebc8afabac42ff68df8363e15e004
teeyingyap <yapteeying@gmail.com>
2017-05-11 10:13:15 +0800

    added default image and datepicker to birthday

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,15 @@
+  document.addEventListener("turbolinks:load",function(){
+
+
+$(document).ready(function () {
+
+        $("#birthday").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            yearRange: '-100:+0'
+            
+        });
+
+    });
+});
+
